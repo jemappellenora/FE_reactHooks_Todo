0e1a1325eea499a6c2b5d2337324f46d668a8507
Ching(Nora) Lau <chingwlau@gmail.com>
2020-10-29 19:06:40 -0400

    feat: toggle to crossOut completed task

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([]);
 
+  const toggleComplete = (i) =>
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    );
+
   return (
     <div className="App">
       <Form
         onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
       />
       <div>
-        {todos.map(({ text }) => (
-          <div key={text}>{text}</div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{ textDecoration: complete ? "line-through" : "" }}
+          >
+            {text}
+          </div>
         ))}
       </div>
     </div>
